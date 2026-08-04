d6989fdcb5c5f98aaab43408696e6444ed3abf86
JasperOwen <jasper.owen31@gmail.com>
2026-08-04 11:34:45 +0100

    Update script.js
    Updated to include Formspree form

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Jasper Owen portfolio loaded.");
+
+window.formspree = window.formspree || function () {
+  (formspree.q = formspree.q || []).push(arguments);
+};
+
+formspree("initForm", {
+  formElement: "#contact-form",
+  formId: "mljrrjon"
+});
